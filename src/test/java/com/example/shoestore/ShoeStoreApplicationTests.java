4bf2f41af500ca34b8d86ad14dd11d8ec682b82d
ristoneeisto <risto.kochevski@gmail.com>
2021-01-04 20:12:15 +0100

    initial commit

---
+++ src/test/java/com/example/shoestore/ShoeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
